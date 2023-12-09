e5771205d47eaf86cd3ee64dc5f3dd5d5b89de5f
ElenaRodionova <93218293+ElenaRodionova@users.noreply.github.com>
2023-12-09 19:13:51 +0000

    new_comand

--- main.py
+++ main.py
@@ -29,4 +29,21 @@ def handle_voice(message):
     # Отправляем ответное сообщение
     bot.send_message(message.chat.id, 'Голосовое сообщение получено')
 
+
+@bot.message_handler(commands=['help'])
+def get_commands(message):
+    commands_list = []
+    for handler in bot.handlers:
+        if handler.command:
+            commands_list.append(f"/{handler.command}")
+    
+    commands_text = "n".join(commands_list)
+    bot.reply_to(message, f"Доступные команды:n{commands_text}")
+    
+@bot.message_handler(commands=['clear_history'])
+def clear_history(message):
+    # Очистить историю запросов
+    bot.clear_update_queue()
+    bot.reply_to(message, "История запросов очищена.")
+
 bot.infinity_polling()
