c6a07157b3f4898db7ea4dbb221ec2247d0a1569
vfliagin <93093512+vfliagin@users.noreply.github.com>
2023-12-10 17:55:49 +0300

    Update model.py
    Адаптация для русского языка

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 #pip install git+https://github.com/openai/whisper.git
 #Установка whisper
 
-import whisper
 import spacy
+from spacy.lang.ru import Russian
 from spacy.lang.ru.stop_words import STOP_WORDS
+import ru_core_news_sm
 from string import punctuation
 from heapq import nlargest
 
@@ -21,10 +22,12 @@ def summarize(text, per):
     """
     Summary текста. Необходимы исправления 
     """
-    nlp = spacy.load('en_core_web_sm')
+    nlp = spacy.load('ru_core_news_sm')
     doc= nlp(text)
     tokens=[token.text for token in doc]
     word_frequencies={}
+    
+    #Вычисление абсолютной частоты слов
     for word in doc:
         if word.text.lower() not in list(STOP_WORDS):
             if word.text.lower() not in punctuation:
@@ -32,9 +35,13 @@ def summarize(text, per):
                     word_frequencies[word.text] = 1
                 else:
                     word_frequencies[word.text] += 1
+
+    #Вычисление относительнйо частоты слов
     max_frequency=max(word_frequencies.values())
     for word in word_frequencies.keys():
         word_frequencies[word]=word_frequencies[word]/max_frequency
+
+    #Вычисление рейтингов предложений
     sentence_tokens= [sent for sent in doc.sents]
     sentence_scores = {}
     for sent in sentence_tokens:
@@ -44,8 +51,10 @@ def summarize(text, per):
                     sentence_scores[sent]=word_frequencies[word.text.lower()]
                 else:
                     sentence_scores[sent]+=word_frequencies[word.text.lower()]
+    
+    #Фильтрация предложений по рейтингу и объединение результата
     select_length=int(len(sentence_tokens)*per)
-    summary=nlargest(select_length, sentence_scores,key=sentence_scores.get)
+    summary=nlargest(select_length, sentence_scores, key=sentence_scores.get)
     final_summary=[word.text for word in summary]
     summary=''.join(final_summary)
 
